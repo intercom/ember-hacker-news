9f9f8502757856c6163b418fcad2aed27d4a0c6e
Lorcan Coyle <lorcan@gmail.com>
2018-04-17 19:40:54 +0100

    Bump ember to 3.1 using ember-cli-update

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,24 @@ module.exports = {
     browser: true
   },
   rules: {
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'ember-cli-build.js',
+        'testem.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    }
+  ]
 };
